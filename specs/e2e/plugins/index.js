28383596f5de3000cd90b4d1cbbd12a30803cd96
Alexandre S <moutardea@gmail.com>
2020-01-28 15:03:02 +0100

    fix path on linux

--- specs/e2e/plugins/index.js
+++ specs/e2e/plugins/index.js
@@ -3,10 +3,10 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber());
   return Object.assign({}, config, {
-    fixturesFolder: 'Specs/e2e/fixtures',
-    integrationFolder: 'Specs/e2e/specs',
-    screenshotsFolder: 'Specs/e2e/screenshots',
-    videosFolder: 'Specs/e2e/videos',
-    supportFile: 'Specs/e2e/support/index.js'
+    fixturesFolder: 'specs/e2e/fixtures',
+    integrationFolder: 'specs/e2e/specs',
+    screenshotsFolder: 'specs/e2e/screenshots',
+    videosFolder: 'specs/e2e/videos',
+    supportFile: 'specs/e2e/support/index.js'
   });
 };
